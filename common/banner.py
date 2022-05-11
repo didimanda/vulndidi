7d588de9902cabbf6c2cc491e1c9efa01cf1084c
didimanda <105385980+didimanda@users.noreply.github.com>
2022-05-12 02:20:29 +0800

    Add files via upload

---
+++ common/banner.py
@@ -0,0 +1,46 @@
+import sys
+from common.colors import bannerblue, bannerblue2, W, Y, R, end
+
+
+def banner():
+    print("""%s
+                                                      
+MMMMMMMMMMMMMMMMMMWWWWMMWWWWWWMMMMMMMMMM
+MMMMMMMMMMMMMMMMWNXKK0OO0KNWWWWMMMMMMMMM
+MMMMMMMMMMMWNX0Okxxxkxdddddd0NWWMMMMMMMM
+MMMMMMMMMWWOl;,''',;:ldkko;,,ckXWMMMMMMM
+MMMMMNKKWWXl.....'.....;c;'''',oKWMMMMMM
+MMWW0dccdk0koll:clccc:'..',,''';xNWMMMMM
+MWWKo:;;::oOxll'.cxxxdol:,'','':kNMMMMMM
+WMW0l:;;;;lxo;;,;okkxkkxxdl:;,,oKWMMMMMM
+MMMNkl:;;;;:coxxxkkxxkkkxkdlc;lONWMMMMMM
+MMWWN0o:;;'.';loddddddxxkko:;cOXWMMMMMMM
+MMMMMWKo;,'...',,;;;;;:::cllokKWMMMMMMMM
+MMMMWMXd:;,'..',,,,,,,,,,,,,:xXWMMMMMMMM
+MMMWWMW0o:;,,,,,,,,,,,,,,,,,;:o0NWMMMMMM
+MMMMMWWk;;;,,,,,,,,,,,,,,,,,,'.;oOXWMMMM
+MMMMMMWKo:;;;,,,,,,,,,,,;,,;,...,:dKWMMM
+MMMMWMMWx,,,',,,,,,,,,;;,,,;cl;'',cONWMM
+MMMMMMMWXkxo:;;,,,',,,,,:c;:xX0l;:dKWMMM
+MMMMMMMMMWWN0kdlllcccc:oOkoo0NWXOOKWMMMM
+MMMMMMMMMMMWX0kolodxkxddxkO0KNWWWWMMMMMM
+MMMMMMMMMMMWNKOkOOOKK0O00KKKXNWWMMMMMMMM
+MMMMMMMMMMMWNXKKKKKKXXXNNNNNWMMMMMMMMMMM
+MMMMMMMMMMMMWWNNNNWWWWMMMMMMMMMMMMMMMMMM   
+
+    %s# Coded By didi -%s @didi      
+                                           %s"""
+          %
+          (bannerblue, bannerblue2,
+           W, bannerblue2, W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2,
+           W, bannerblue2, W, bannerblue2,
+           W, bannerblue2,
+           W, bannerblue2,
+           W, Y, end
+           ))
